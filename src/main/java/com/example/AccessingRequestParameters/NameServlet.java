6ecf5dc34a1611f63832aceeda493f0bdc1feb6d
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2021-05-25 17:45:22 -0400

    Initial commit

---
+++ src/main/java/com/example/AccessingRequestParameters/NameServlet.java
@@ -0,0 +1,30 @@
+package com.example.AccessingRequestParameters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+//http://localhost:8080/name?firstName=Amir&lastName=Shah
+
+@WebServlet(name = "NameServlet", urlPatterns = "/name")
+public class NameServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+
+        System.out.println(firstName +" " + lastName);
+        if (firstName == null || lastName == null) {
+            System.out.println("A full name is not provided");
+        } else {
+            System.out.println("Hello " + firstName + " " + lastName + ", nice to meet you");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
